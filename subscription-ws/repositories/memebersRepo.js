7a5d1e9f69c9a15474698bed216bc3b2e936cb32
TomBer101 <tomerm1233@gmail.com>
2024-09-14 15:41:30 +0300

    repositories
    added movies and memebers repo to populate data

--- subscription-ws/repositories/memebersRepo.js
+++ subscription-ws/repositories/memebersRepo.js
@@ -1,3 +1,12 @@
 const axios = require('axios')
 
 const URL = "https://jsonplaceholder.typicode.com/users"
+
+const populateMemebers = () => {
+    const response =  axios.get(URL)
+    return response
+}
+
+module.exports = {
+    populateMemebers
+}
--- subscription-ws/repositories/moviesRepo.js
+++ subscription-ws/repositories/moviesRepo.js
@@ -1,3 +1,15 @@
 const axios = require('axios')
 
 const URL = "https://api.tvmaze.com/shows"
+
+const populateMovies = async () => {
+    const {data: movies} = await axios.get(URL)
+    return movies.map(movie => ({
+        ...movie, 
+        image: movie.image.medium
+    }))
+}
+
+module.exports = {
+    populateMovies,
+}
