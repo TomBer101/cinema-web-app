14c7e3a77e66a96e360ce1032ea3f53749106889
TomBer101 <tomerm1233@gmail.com>
2024-10-08 11:16:08 +0300

    Update index.js
    added express-session middleware

--- cinema-ws/index.js
+++ cinema-ws/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
-
+const session = require('express-session')
 const configDB = require('./mongoDB/config')
+
+const authRouter = require('./routes/authRouter')
 
 const PORT = 8080;
 const app = express();
 configDB.connectDB();
+
+app.use(express.json())
+
+app.use(session({
+    secret: 'dummy-secret',   // Replace with a secure key
+    resave: false,
+    saveUninitialized: false,
+        // Default session duration (e.g., 1 minute), can be adjusted
+}));
+
+app.use('/api/auth', authRouter)
+
+
 
 
 app.listen(PORT, async () => {
