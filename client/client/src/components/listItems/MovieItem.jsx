622142d3aa33ed8d3587a1164a433d318aa7da04
TomBer101 <tomerm1233@gmail.com>
2024-11-21 22:47:52 +0200

    movie item

--- client/client/src/components/listItems/MovieItem.jsx
+++ client/client/src/components/listItems/MovieItem.jsx
@@ -1,7 +1,26 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import { useDeleteMovie } from '../../hooks/useMoviesMutations';
+import { useNavigate } from 'react-router-dom';
 
 const MovieItem = ({id, name, generes, image, premiered, subscriptions}) => {
+    const {mutate: deleteMovie} = useDeleteMovie()
+    const navigate = useNavigate()
+
+    const handleDelete = (e) => {
+        e.preventDefault()
+        deleteMovie(id)
+    };
+
+    const handleEditOnClick = () => {
+        const state = {
+            id, name, generes, image, premiered, subscriptions 
+        }
+
+        navigate('edit', {state})
+    }
+
+
     return (
         <Box sx={{
             display: 'flex',
